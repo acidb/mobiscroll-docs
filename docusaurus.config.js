49c5494a4ae163687d982173a6be7a43d6dd3e31
azandrasz <azandrasz@gmail.com>
2023-02-17 13:55:27 +0100

    initial content and dummy version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           includeCurrentVersion: false,
-          lastVersion: '5.23.0',
+          lastVersion: '5.22.0',
         },
         blog: false,
         theme: {

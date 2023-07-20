e8b3f9a0f1868382c8ea5ce5af22c12ac19c176d
Halmen Istvan <isti@mobiscroll.com>
2023-07-20 16:33:48 +0300

    Update current version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           includeCurrentVersion: false,
-          lastVersion: '5.25.0',
+          lastVersion: '5.26.0',
         },
         blog: false,
         theme: {

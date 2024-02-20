7b49ade12d789abcf56fa96f8f22572de2c7438a
azandrasz <azandrasz@gmail.com>
2024-02-20 08:54:24 +0100

    update

--- versioned_docs/version-5.29.0/javascript/links.js
+++ versioned_docs/version-5.29.0/javascript/links.js
@@ -24,7 +24,7 @@ const config = {
     {
       type: 'link',
       label: 'Styling & theming',
-      href: '/javascript/theming/build-in-themes',
+      href: '/javascript/theming/built-in-themes',
       description: 'How to customize the look'
       // icon: '/img/javascript-logo.svg',
     },
--- versioned_docs/version-5.29.0/vue/links.js
+++ versioned_docs/version-5.29.0/vue/links.js
@@ -24,7 +24,7 @@ const config = {
     {
       type: 'link',
       label: 'Styling & theming',
-      href: '/vue/theming/build-in-themes',
+      href: '/vue/theming/built-in-themes',
       description: 'How to customize the look'
       // icon: '/img/javascript-logo.svg',
     },

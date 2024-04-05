377e316ea0caf82a3039f70cad08b91bdb265248
azandrasz <azandrasz@gmail.com>
2024-04-05 13:00:59 +0200

    syncing TOC active menu based on scroll

--- src/theme/DocPage/index.js
+++ src/theme/DocPage/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import DocPage from "@theme-original/DocPage";
 import { useLocation, useHistory } from "@docusaurus/router";
-import { showActiveMenu } from "@site/src/util/menuscroll";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import { captureTOCPositions, highlightTOC, showActiveMenu } from "@site/src/util/menuscroll";
 
 export default function DocPageWrapper(props) {
   const { pathname, hash } = useLocation();
@@ -14,6 +15,33 @@ export default function DocPageWrapper(props) {
 
     setTimeout(() => showActiveMenu(), 50);
   }, []);
+
+  const isBrowser = useIsBrowser();
+
+  React.useEffect(() => {
+    // reload TOC's on every page navigation
+    captureTOCPositions();
+  }, [pathname]);
+
+  // Scroll listener
+  React.useEffect(() => {
+    let requestRunning = null;
+    function handleScroll() {
+      if (isBrowser && requestRunning === null) {
+        requestRunning = window.requestAnimationFrame(() => {
+          // highlight the right TOC item based on the window scroll
+          highlightTOC(window.scrollY);
+          requestRunning = null;
+        });
+      }
+    }
+
+    if (isBrowser) {
+      window.addEventListener("scroll", handleScroll, { passive: true });
+      return () => window.removeEventListener("scroll", handleScroll, { passive: true });
+    }
+  }, [isBrowser]);
+
   return (
     <>
       <DocPage {...props} />
--- src/util/menuscroll.ts
+++ src/util/menuscroll.ts
@@ -11,3 +11,54 @@ export function showActiveMenu() {
     });
   }
 }
+
+let tocPositions: Array<{element: HTMLElement, targetPosition: number }> = [];
+
+/**
+ * Reads the TOC links and calculates the scroll positions
+ */
+export function captureTOCPositions() {
+  tocPositions = [];
+  const toc = document.querySelectorAll<HTMLElement>(".table-of-contents__link");
+  for (let i = 0; i < toc.length; i++) {
+    const el = toc[i];
+    const target = document.getElementById(
+      el.getAttribute("href").replace("#", ""),
+    );
+    if (target) {
+      tocPositions.push({
+        element: el,
+        targetPosition: target.offsetTop,
+      });
+    }
+  }
+}
+
+/**
+ * Highlights the active item in the TOC based on the scroll position
+ * @param scrollY The scroll position based on which the active item is calculated
+ */
+export function highlightTOC(scrollY) {
+  for (let i = 0; i < tocPositions.length; i++) {
+    tocPositions[i].element.classList.remove("table-of-contents__link--active");
+  }
+  let activeIndex = -1;
+  if (
+    tocPositions.length > 0 &&
+    tocPositions[tocPositions.length - 1].targetPosition < scrollY
+  ) {
+    activeIndex = tocPositions.length - 1;
+  } else {
+    for (let i = 0; i < tocPositions.length; i++) {
+      if (tocPositions[i].targetPosition > scrollY) {
+        activeIndex = i - 1;
+        break;
+      }
+    }
+  }
+  if (activeIndex > -1 && activeIndex < tocPositions.length) {
+    tocPositions[activeIndex].element.classList.add(
+      "table-of-contents__link--active",
+    );
+  }
+}

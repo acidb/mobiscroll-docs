a91cf70ced6d054d618a2bbf4f36ef62e36b0704
azandrasz <azandrasz@gmail.com>
2024-12-04 10:40:29 +0200

    hiding table rows interactively

---
+++ src/util/tables.ts
@@ -0,0 +1,27 @@
+
+/**
+ * Should be used only client side
+ */
+export function initializeHideSwitch() {
+  const clickHandler = (ev: Event) => {
+    const target = ev.target as HTMLElement;
+    if (target && target.classList && target.classList.length) {
+      if (target.classList.contains('table-hide-switch')) {
+        const sw = target as HTMLInputElement;
+        const isOn = sw.checked;
+        const table = sw.closest('table');
+        if (table) {
+          table.querySelectorAll('.js-not-applicable').forEach((tr) => {
+            if (isOn) {
+              tr.classList.remove('table-row-hidden');
+            } else {
+              tr.classList.add('table-row-hidden');
+            }
+          });
+        }
+      }
+    }
+  }
+  window.addEventListener('click', clickHandler);
+  return () => { window.removeEventListener('click', clickHandler); }
+}

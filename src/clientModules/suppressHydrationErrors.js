fefb9034d0f46bd98d121d3f1f3423575d31977e
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2026-06-18 17:18:51 +0300

    Revert "fix(iis): suppress React 18 hydration errors caused by MVC URL routing"
    This reverts commit 0d2a31fed19952591f444ae25a807e391d5c7f51.

--- src/clientModules/suppressHydrationErrors.js
+++
@@ -1,9 +0,0 @@
-if (typeof window !== 'undefined') {
-  const originalError = console.error.bind(console);
-  console.error = (...args) => {
-    if (typeof args[0] === 'string' && args[0].includes('Docusaurus React Root onRecoverableError')) {
-      return;
-    }
-    originalError(...args);
-  };
-}
